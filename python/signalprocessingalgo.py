e241c45d6d2bf266d115b84f140a35446ffcd80b
Selena Zhou <selena.zhou@mongodb.com>
2025-07-24 15:22:18 -0400

    Update tests in signal processing algos and delete old data

--- python/signalprocessingalgo.py
+++ python/signalprocessingalgo.py
@@ -1,11 +1,11 @@
 from signal_processing_algorithms.energy_statistics import energy_statistics
 import numpy as np
 
-x = np.random.rand(100, 5)
-y = np.random.rand(100, 5)
+v1 = [50, 52, 53, 50, 48, 49, 52]
+v2 = [100, 50, 52, 48]
+
+x = np.array(v1)
+y = np.array(v2)
 
 energy_stats = energy_statistics.get_energy_statistics_and_probabilities(x, y)
-change_points = energy_statistics.e_divisive(x)
-
 print(energy_stats)
-print(change_points)
